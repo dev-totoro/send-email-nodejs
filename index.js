40ff6bf05522c76da4ab7e772b5b99625480fcc3
Totoro Dev <demo.totoro.dev@gmail.com>
2021-09-04 12:34:49 +0700

    Update route

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 require('dotenv').config()
-const express = require('express')
+const express = require('express');
+const mailer = require('./utils/mail');
 
-const app = express()
+const app = express();
 
 const port = process.env.PORT;
 
+mailer.init();
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const receivedEmail = req.query.email;
+  const title = "Test title";
+  const body = "Test content";
+  mailer.sendMail(receivedEmail,title,body);
+  res.send(`An email was sent to ${receivedEmail}!`)
 })
 
 app.listen(port, () => {
